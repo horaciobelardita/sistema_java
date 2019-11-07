ee215c4f3bc582d0583a3d86f848453063e19e0a
Belardita Horacio <horaciobelardita@gmail.com>
2019-11-07 17:55:00 -0300

    metodo convertir coma a punto

--- src/utils/Helper.java
+++ src/utils/Helper.java
@@ -19,11 +19,23 @@ public class Helper {
         if (evt.getKeyChar() >= '0' && evt.getKeyChar() <= '9' || 
                 evt.getKeyChar() == KeyEvent.VK_DELETE ||
                  evt.getKeyChar() == KeyEvent.VK_BACK_SPACE ||
-                evt.getKeyChar() == KeyEvent.VK_PERIOD
+                evt.getKeyChar() == KeyEvent.VK_COMMA
                 ) {
             campo.setEditable(true);
         } else {
             campo.setEditable(false);
         }
+    }
+    
+    public static String convertirComaAPunto(String valor) {
+        String retorno = "";
+        for (int i = 0; i < valor.length(); i++) {
+            if (valor.charAt(i) == ',') {
+                retorno += ".";
+            } else {
+                retorno += valor.charAt(i);
+            }
+        }
+        return retorno;
     }
 }
