e74713b6506645572b6cda324bd105744562a5eb
Horacio Belardita <7333656+horaciobelardita@users.noreply.github.com>
2019-11-07 11:24:45 -0300

    validar solo numero permitir tecla backspace

--- src/utils/Helper.java
+++ src/utils/Helper.java
@@ -1,11 +1,13 @@
 package utils;
 
+import java.awt.event.KeyEvent;
 import javax.swing.JTextField;
 
 public class Helper {
 
     public static void validarSoloNumero(java.awt.event.KeyEvent evt, JTextField campo) {
-        if (evt.getKeyChar() >= '0' && evt.getKeyChar() <= '9') {
+        if (evt.getKeyChar() >= '0' && evt.getKeyChar() <= '9' || 
+                evt.getKeyChar() == KeyEvent.VK_DELETE) {
             campo.setEditable(true);
         } else {
             campo.setEditable(false);
