bf4b276e699e9866e28c33cefb3f59cb37193ae6
Belardita Horacio <7333656+horaciobelardita@users.noreply.github.com>
2019-11-08 09:14:41 -0300

    validar campo alfanumerico y permitir barra espaciadora

--- src/utils/Helper.java
+++ src/utils/Helper.java
@@ -6,39 +6,38 @@ import javax.swing.JTextField;
 public class Helper {
 
     public static void validarSoloNumero(java.awt.event.KeyEvent evt, JTextField campo) {
-        if (evt.getKeyChar() >= '0' && evt.getKeyChar() <= '9' || 
-                evt.getKeyChar() == KeyEvent.VK_DELETE || 
-                evt.getKeyChar() == KeyEvent.VK_BACK_SPACE) {
+        if (evt.getKeyChar() >= '0' && evt.getKeyChar() <= '9'
+                || evt.getKeyChar() == KeyEvent.VK_DELETE
+                || evt.getKeyChar() == KeyEvent.VK_BACK_SPACE) {
             campo.setEditable(true);
         } else {
             campo.setEditable(false);
         }
     }
-    
+
     public static void validarSoloNumeroDecimales(java.awt.event.KeyEvent evt, JTextField campo) {
-        if (evt.getKeyChar() >= '0' && evt.getKeyChar() <= '9' || 
-                evt.getKeyChar() == KeyEvent.VK_DELETE ||
-                 evt.getKeyChar() == KeyEvent.VK_BACK_SPACE ||
-                evt.getKeyChar() == KeyEvent.VK_COMMA
-                ) {
+        if (evt.getKeyChar() >= '0' && evt.getKeyChar() <= '9'
+                || evt.getKeyChar() == KeyEvent.VK_DELETE
+                || evt.getKeyChar() == KeyEvent.VK_BACK_SPACE
+                || evt.getKeyChar() == KeyEvent.VK_COMMA) {
             campo.setEditable(true);
         } else {
             campo.setEditable(false);
         }
     }
-    
-     public static void validarAlphanumerico(java.awt.event.KeyEvent evt, JTextField campo) {
-        if (evt.getKeyChar() >= 'a' && evt.getKeyChar() <= 'z' || 
-                evt.getKeyChar() >= 'A' && evt.getKeyChar() <= 'Z' || 
-                evt.getKeyChar() == KeyEvent.VK_DELETE ||
-                 evt.getKeyChar() == KeyEvent.VK_BACK_SPACE 
-                ) {
+
+    public static void validarAlphanumerico(java.awt.event.KeyEvent evt, JTextField campo) {
+        if (evt.getKeyChar() >= 'a' && evt.getKeyChar() <= 'z'
+                || evt.getKeyChar() >= 'A' && evt.getKeyChar() <= 'Z'
+                || evt.getKeyChar() == KeyEvent.VK_DELETE
+                || evt.getKeyChar() == KeyEvent.VK_BACK_SPACE
+                || evt.getKeyChar() == KeyEvent.VK_SPACE) {
             campo.setEditable(true);
         } else {
             campo.setEditable(false);
         }
     }
-    
+
     public static String convertirComaAPunto(String valor) {
         String retorno = "";
         for (int i = 0; i < valor.length(); i++) {
@@ -50,8 +49,8 @@ public class Helper {
         }
         return retorno;
     }
-    
-        public static String convertirPuntoAComa(String valor) {
+
+    public static String convertirPuntoAComa(String valor) {
         String retorno = "";
         for (int i = 0; i < valor.length(); i++) {
             if (valor.charAt(i) == '.') {
