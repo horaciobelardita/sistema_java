e3b78d4e3542abf1e330e83a473da816ae2ee7ac
Belardita Horacio <horaciobelardita@gmail.com>
2019-11-23 17:46:07 -0300

    validaciones login

--- src/FramesPanels/inicio.java
+++ src/FramesPanels/inicio.java
@@ -1,27 +1,29 @@
 package FramesPanels;
 
+import db.MetodosSQL;
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import javax.swing.JOptionPane;
-import db.MetodosSQL;
 import modelos.Usuario;
+import utils.Helper;
 
 public class inicio extends javax.swing.JFrame {
 
-    public inicio() {
-        initComponents();
-        setLocationRelativeTo(null);
-        txtNick.setText("root");
-        txtPassword.setText("root");
-    }
+  public inicio() {
+    initComponents();
+    setLocationRelativeTo(null);
+    txtNick.setText("root");
+    txtPassword.setText("root");
+  }
 
-    @SuppressWarnings("unchecked")
+  @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         contenedor_lo = new javax.swing.JPanel();
         jpanel_main = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+        lblNombreUsuario = new javax.swing.JLabel();
+        lblContraseña = new javax.swing.JLabel();
         jSeparator4 = new javax.swing.JSeparator();
         jSeparator5 = new javax.swing.JSeparator();
         txtPassword = new javax.swing.JPasswordField();
@@ -37,6 +39,7 @@ public class inicio extends javax.swing.JFrame {
         jLabel10 = new javax.swing.JLabel();
         btn_regis1 = new javax.swing.JPanel();
         jLabel11 = new javax.swing.JLabel();
+        lblMensaje = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -47,21 +50,22 @@ public class inicio extends javax.swing.JFrame {
         jpanel_main.setBackground(new java.awt.Color(47, 34, 23));
         jpanel_main.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel4.setBackground(new java.awt.Color(47, 34, 23));
-        jLabel4.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setText("NOMBRE DE USUARIO");
-        jpanel_main.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 140, -1, -1));
+        lblNombreUsuario.setBackground(new java.awt.Color(47, 34, 23));
+        lblNombreUsuario.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
+        lblNombreUsuario.setForeground(new java.awt.Color(255, 255, 255));
+        lblNombreUsuario.setText("NOMBRE DE USUARIO");
+        jpanel_main.add(lblNombreUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 140, -1, -1));
 
-        jLabel5.setBackground(new java.awt.Color(47, 34, 23));
-        jLabel5.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setText("CONTRASEÑA");
-        jpanel_main.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 210, -1, -1));
+        lblContraseña.setBackground(new java.awt.Color(47, 34, 23));
+        lblContraseña.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
+        lblContraseña.setForeground(new java.awt.Color(255, 255, 255));
+        lblContraseña.setText("CONTRASEÑA");
+        jpanel_main.add(lblContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 210, -1, -1));
         jpanel_main.add(jSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 190, 230, 10));
         jpanel_main.add(jSeparator5, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 260, 230, 10));
 
         txtPassword.setBackground(new java.awt.Color(47, 34, 23));
+        txtPassword.setFont(new java.awt.Font("Fira Sans Semi-Light", 0, 18)); // NOI18N
         txtPassword.setForeground(new java.awt.Color(255, 255, 255));
         txtPassword.setText("jPasswordField1");
         txtPassword.setBorder(null);
@@ -75,11 +79,14 @@ public class inicio extends javax.swing.JFrame {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 txtPasswordKeyPressed(evt);
             }
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtPasswordKeyReleased(evt);
+            }
         });
         jpanel_main.add(txtPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 230, 230, 30));
 
         txtNick.setBackground(new java.awt.Color(47, 34, 23));
-        txtNick.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        txtNick.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
         txtNick.setForeground(new java.awt.Color(255, 255, 255));
         txtNick.setText("Ingresa tu nick");
         txtNick.setToolTipText("");
@@ -104,11 +111,14 @@ public class inicio extends javax.swing.JFrame {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 txtNickKeyPressed(evt);
             }
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtNickKeyReleased(evt);
+            }
         });
         jpanel_main.add(txtNick, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 160, 230, 30));
 
         btn_regis.setBackground(new java.awt.Color(255, 102, 0));
-        btn_regis.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_regis.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btn_regis.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 btn_regisMouseClicked(evt);
@@ -163,7 +173,7 @@ public class inicio extends javax.swing.JFrame {
         jpanel_main.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 350, 500));
 
         btn_regis1.setBackground(new java.awt.Color(255, 102, 0));
-        btn_regis1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btn_regis1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btn_regis1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 btn_regis1MouseClicked(evt);
@@ -181,6 +191,10 @@ public class inicio extends javax.swing.JFrame {
 
         jpanel_main.add(btn_regis1, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 310, 90, 40));
 
+        lblMensaje.setFont(new java.awt.Font("Fira Sans Semi-Light", 0, 18)); // NOI18N
+        lblMensaje.setForeground(java.awt.Color.white);
+        jpanel_main.add(lblMensaje, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 380, 330, -1));
+
         contenedor_lo.add(jpanel_main, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 700, 500));
 
         getContentPane().add(contenedor_lo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 700, 500));
@@ -189,7 +203,7 @@ public class inicio extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void txtPasswordFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtPasswordFocusGained
-        txtPassword.setText("");
+    txtPassword.setText("");
     }//GEN-LAST:event_txtPasswordFocusGained
 
     private void txtNickMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtNickMouseClicked
@@ -197,87 +211,107 @@ public class inicio extends javax.swing.JFrame {
     }//GEN-LAST:event_txtNickMouseClicked
 
     private void txtNickActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNickActionPerformed
-        // TODO add your handling code here:
+    // TODO add your handling code here:
     }//GEN-LAST:event_txtNickActionPerformed
 
     private void btn_regisMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_regisMouseClicked
-        validar();
-
+    validar();
 
     }//GEN-LAST:event_btn_regisMouseClicked
 
     private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
-        int dialog = JOptionPane.YES_NO_OPTION;
-        int result = JOptionPane.showConfirmDialog(null, "Desea cerrar el programa?", "Exit", dialog);
-        if (result == 0) {
-            System.exit(0);
-        }
+    int dialog = JOptionPane.YES_NO_OPTION;
+    int result = JOptionPane.showConfirmDialog(null, "Desea cerrar el programa?", "Exit", dialog);
+    if (result == 0) {
+      System.exit(0);
+    }
     }//GEN-LAST:event_jButton2MouseClicked
 
     private void btn_regisMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_regisMouseEntered
-        // TODO add your handling code here:
+    // TODO add your handling code here:
     }//GEN-LAST:event_btn_regisMouseEntered
 
     private void txtPasswordKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPasswordKeyPressed
-        // TODO add your handling code here:
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            btn_regisMouseClicked(null);
-        }
+    // TODO add your handling code here:
+    if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+      btn_regisMouseClicked(null);
+    }
     }//GEN-LAST:event_txtPasswordKeyPressed
 
     private void txtNickKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtNickKeyPressed
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            txtPassword.requestFocus();
-        }
+    if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+      txtPassword.requestFocus();
+    }
     }//GEN-LAST:event_txtNickKeyPressed
 
     private void txtNickFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtNickFocusGained
-        txtNick.setText("");
-        // TODO add your handling code here:
+    txtNick.setText("");
+    // TODO add your handling code here:
     }//GEN-LAST:event_txtNickFocusGained
 
     private void btn_regis1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_regis1MouseClicked
-        System.exit(0);
+    System.exit(0);
     }//GEN-LAST:event_btn_regis1MouseClicked
 
     private void btn_regis1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_regis1MouseEntered
-        // TODO add your handling code here:
+    // TODO add your handling code here:
     }//GEN-LAST:event_btn_regis1MouseEntered
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new inicio().setVisible(true);
-            }
-        });
+    private void txtNickKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtNickKeyReleased
+    if (Helper.esCampoVacio(txtNick)) {
+      Helper.mostrarError(txtNick, lblNombreUsuario, "NOMBRE DE USUARIO", new Color(255, 102, 0));
+      lblMensaje.setText("Ingrese un usuario");
+    } else {
+      lblNombreUsuario.setForeground(Color.white);
+      lblMensaje.setText("");
     }
+    }//GEN-LAST:event_txtNickKeyReleased
+
+    private void txtPasswordKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPasswordKeyReleased
+    String password = new String(txtPassword.getPassword());
+    if (password.isEmpty()) {
+      Helper.mostrarError(txtPassword, lblContraseña, "CONTRASEÑA", new Color(255, 102, 0));
+      lblMensaje.setText("Ingrese su contraseña");
+    } else {
+      lblContraseña.setForeground(Color.white);
+      lblMensaje.setText("");
+    }
+    }//GEN-LAST:event_txtPasswordKeyReleased
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String args[]) {
+    /* Set the Nimbus look and feel */
+    //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+    /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+     */
+    try {
+      for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+        if ("Nimbus".equals(info.getName())) {
+          javax.swing.UIManager.setLookAndFeel(info.getClassName());
+          break;
+        }
+      }
+    } catch (ClassNotFoundException ex) {
+      java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    } catch (InstantiationException ex) {
+      java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    } catch (IllegalAccessException ex) {
+      java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+      java.util.logging.Logger.getLogger(inicio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
+    //</editor-fold>
+
+    /* Create and display the form */
+    java.awt.EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        new inicio().setVisible(true);
+      }
+    });
+  }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel btn_regis;
@@ -288,8 +322,6 @@ public class inicio extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JSeparator jSeparator2;
@@ -297,32 +329,35 @@ public class inicio extends javax.swing.JFrame {
     private javax.swing.JSeparator jSeparator4;
     private javax.swing.JSeparator jSeparator5;
     private javax.swing.JPanel jpanel_main;
+    private javax.swing.JLabel lblContraseña;
+    private javax.swing.JLabel lblMensaje;
+    private javax.swing.JLabel lblNombreUsuario;
     private javax.swing.JTextField txtNick;
     private javax.swing.JPasswordField txtPassword;
     // End of variables declaration//GEN-END:variables
 
-    private void validar() {
-        String nombreUsuario = txtNick.getText();
-        String password = txtPassword.getText();
-        if (nombreUsuario.isEmpty() || password.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Ingrese Usuario y contraseña.",
-                    "Error: Ingrese sus datos de usuario", JOptionPane.ERROR_MESSAGE);
-            txtNick.requestFocus();
-            return;
-        }
-
-        Usuario usuario = MetodosSQL.buscarUsuario(txtNick.getText(), txtPassword.getText());
-
-        if (usuario.getNombre() != null && usuario.getId() != null) {
-            JOptionPane.showMessageDialog(this, "Bienvenid@ \n" + usuario.getNick());
-            Home Home = new Home();
-            Home.setVisible(true);
-            Home.jl_nombre.setText(usuario.getNick());
-            this.dispose();
-        } else {
-            JOptionPane.showMessageDialog(this, "Datos incorrectos: verifique su Nombre de usuario y contraseña\n",
-                    "Error: Datos no Validos", JOptionPane.ERROR_MESSAGE);
-        }
+  private void validar() {
+    String nombreUsuario = txtNick.getText();
+    String password = new String(txtPassword.getPassword());
+    if (nombreUsuario.isEmpty() || password.isEmpty()) {
+      JOptionPane.showMessageDialog(this, "Ingrese Usuario y contraseña.",
+        "Error: Ingrese sus datos de usuario", JOptionPane.ERROR_MESSAGE);
+      txtNick.requestFocus();
+      return;
     }
+
+    Usuario usuario = MetodosSQL.buscarUsuario(nombreUsuario, password);
+
+    if (usuario.getNombre() != null && usuario.getId() != null) {
+      JOptionPane.showMessageDialog(this, "Bienvenid@ \n" + usuario.getNick());
+      Home Home = new Home();
+      Home.setVisible(true);
+      Home.jl_nombre.setText(usuario.getNick());
+      this.dispose();
+    } else {
+      JOptionPane.showMessageDialog(this, "Datos incorrectos: verifique su Nombre de usuario y contraseña\n",
+        "Error: Datos no Validos", JOptionPane.ERROR_MESSAGE);
+    }
+  }
 
 }
